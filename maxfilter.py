711fdc0f7afee5857a8f0cdda208cfc606bc8ded
aurore.bussalb <aurore.bussalb@mensiatech.com>
2021-03-25 10:20:04 +0100

    check of doctrings and comments

--- maxfilter.py
+++ maxfilter.py
@@ -20,8 +20,8 @@ def maxfilter(raw, calibration_file, cross_talk_file, head_pos_file, destination
         Path to the '.dat' file with fine calibration coefficients. This file is machine/site-specific.
     cross_talk_file: str or None
         Path to the FIF file with cross-talk correction information.
-    head_pos_file: array or None
-        If array, movement compensation will be performed.
+    head_pos_file: str or None
+        Path to the '.pos' file containing the info to perform movement compensation.
     destination_file: str or None
         The destination location for the head. Can be None, which will not change the head position, or a string path
         to a FIF file containing a MEG device<->head transformation.
@@ -35,15 +35,15 @@ def maxfilter(raw, calibration_file, cross_talk_file, head_pos_file, destination
     param_ext_order: int
         Order of external component of spherical expansion.
     param_coord_frame: str
-        The coordinate frame that the origin is specified in, either 'meg' or 'head'.
+        The coordinate frame that the origin is specified in, either 'meg' or 'head' (default).
     param_regularize: str or None
-        Basis regularization type, must be “in” or None.
+        Basis regularization type, must be “in” (default) or None.
     param_ignore_ref: bool
         If True, do not include reference channels in compensation.
     param_bad_condition: str
         How to deal with ill-conditioned SSS matrices. Can be “error” (default), “warning”, “info”, or “ignore”.
     param_st_fixed: bool
-        If True (default), do tSSS using the median head position during the param_st_duration window.
+        If True (default), do spatiotemporal SSS using the median head position during the param_st_duration window.
     param_st_only: bool
         If True, only tSSS (temporal) projection of MEG data will be performed on the output data.
     param_skip_by_annotation: str or list of str
@@ -92,25 +92,25 @@ def maxfilter(raw, calibration_file, cross_talk_file, head_pos_file, destination
 def _compute_snr(meg_file):
     # Compute the SNR
 
-    # select only MEG channels and exclude the bad channels
+    # Select only MEG channels and exclude the bad channels
     meg_file = meg_file.pick_types(meg=True, exclude='bads')
 
-    # create fixed length events
+    # Create fixed length events
     array_events = mne.make_fixed_length_events(meg_file, duration=10)
 
-    # create epochs
+    # Create epochs
     epochs = mne.Epochs(meg_file, array_events)
 
-    # mean signal amplitude on each epoch
+    # Compute mean signal amplitude on each epoch
     epochs_data = epochs.get_data()
     mean_signal_amplitude_per_epoch = epochs_data.mean(axis=(1, 2))  # mean on channels and times
 
-    # mean across all epochs and its std error
+    # Compute mean across all epochs and its std error
     mean_final = mean_signal_amplitude_per_epoch.mean()
     std_error_final = np.std(mean_signal_amplitude_per_epoch, ddof=1) / np.sqrt(
         np.size(mean_signal_amplitude_per_epoch))
 
-    # compute SNR
+    # Compute SNR
     snr = mean_final / std_error_final
 
     return snr
@@ -119,7 +119,7 @@ def _compute_snr(meg_file):
 def _generate_report(data_file_before, raw_before_preprocessing, raw_after_preprocessing, bad_channels, snr_before, snr_after):
     # Generate a report
 
-    # Instance of mne.Report
+    # Create instance of mne.Report
     report = mne.Report(title='Results Maxfilter', verbose=True)
 
     # Plot MEG signals in temporal domain
@@ -133,7 +133,6 @@ def _generate_report(data_file_before, raw_before_preprocessing, raw_after_prepr
     fig_raw_maxfilter_psd = raw_after_preprocessing.plot_psd()
 
     # Add figures to report
-    # Add figures to report
     report.add_figs_to_section(fig_raw, captions='MEG signals before MaxFilter', section='Temporal domain')
     report.add_figs_to_section(fig_raw_maxfilter, captions='MEG signals after MaxFilter', section='Temporal domain')
     report.add_figs_to_section(fig_raw_psd, captions='Power spectral density before MaxFilter',
@@ -141,7 +140,6 @@ def _generate_report(data_file_before, raw_before_preprocessing, raw_after_prepr
     report.add_figs_to_section(fig_raw_maxfilter_psd, captions='Power spectral density after MaxFilter',
                                section='Frequency domain')
 
-    # Put this info in html format
     # Give some info about the file before preprocessing
     sampling_frequency = raw_before_preprocessing.info['sfreq']
     highpass = raw_before_preprocessing.info['highpass']
@@ -150,7 +148,6 @@ def _generate_report(data_file_before, raw_before_preprocessing, raw_after_prepr
     # Put this info in html format
     # Info on data
     html_text_info = f"""<html>
-
     <head>
         <style type="text/css">
             table {{ border-collapse: collapse;}}
@@ -182,7 +179,6 @@ def _generate_report(data_file_before, raw_before_preprocessing, raw_after_prepr
 
     # Info on SNR
     html_text_snr = f"""<html>
-
     <head>
         <style type="text/css">
             table {{ border-collapse: collapse;}}
@@ -242,7 +238,7 @@ def main():
     else:
         destination_file = None
 
-    # Head pos file
+    # Get head pos file
     if 'headshape' in config.keys():
         head_pos_file = config.pop('headshape')
         if head_pos_file is not None:  # when App is run locally and "head_position": null in config.json
@@ -252,11 +248,11 @@ def main():
 
     # Check if param_st_duration is not None
     if config['param_st_duration'] == "":
-        param_st_duration = None
+        param_st_duration = None  # when App is run on Bl, no value for this parameter corresponds to ''
     else:
         param_st_duration = config['param_st_duration']
 
-    # Warning if bad channels are empty
+    # Display a warning if bad channels are empty
     if not raw.info['bads']:
         user_warning_message = f'No channels are marked as bad. ' \
                                f'Make sure to check (automatically or visually) for bad channels before ' \
@@ -264,14 +260,17 @@ def main():
         warnings.warn(user_warning_message)
         dict_json_product['brainlife'].append({'type': 'warning', 'msg': user_warning_message})
 
+    # Keep bad channels in memory before they are interpolated by MaxFilter
     bad_channels = raw.info['bads']
+
+    # Apply MaxFilter
     raw_maxfilter = maxfilter(raw, calibration_file, cross_talk_file, head_pos_file, destination_file,
                               param_st_duration, config['param_st_correlation'], config['param_int_order'],
                               config['param_ext_order'], config['param_coord_frame'], config['param_regularize'],
                               config['param_ignore_ref'], config['param_bad_condition'], config['param_st_fixed'],
                               config['param_st_only'], config['param_skip_by_annotation'], config['param_mag_scale'])
 
-    # Success message in product.json
+    # Write a success message in product.json
     dict_json_product['brainlife'].append({'type': 'success', 'msg': 'MaxFilter was applied successfully.'})
 
     # Compute SNR
